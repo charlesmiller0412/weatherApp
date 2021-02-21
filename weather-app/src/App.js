29212ca28080fb151a2b94c27442028f15ca8f7b
Charles Miller <charles.miller.0412@gmail.com>
2021-02-21 00:37:57 -0600

    partial complete. Needs validation. Added images and 5-day.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import React, { useState } from 'react';
 const api = {
-    // key: "",
-    base: "https://api.openweathermap.org/data/2.5/"
+    key: "",
+    base: "https://api.openweathermap.org/data/2.5/",
   }
 
 
 function App() {
-
-  //GET LOCATION
-  // navigator.geolocation.getCurrentPosition();
 
   // ACTIONS TO MOVE TO RESULTS SCREEN
   const [home, showHome] = useState(true);
@@ -18,20 +15,43 @@ function App() {
 
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+  const [futureWeather, setFuture] = useState({});
 
-
-  // SEARCH FUNCTION
+  // Home Button
+  const goHome = evt => {
+    showHome(true); showNav(false); showResults(false);
+  }
+  // SEARCH FUNCTIONS
   const search = evt => {
     if (evt.key === "Enter") {
+      fetch(`${api.base}forecast?q=${query},US&units=imperial&APPID=${api.key}`)
+        .then(res =>res.json())
+        .then(future => {
+          setFuture(future);
+        })
       fetch(`${api.base}weather?q=${query},US&units=imperial&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          showHome(false); showNav(true); showResults(true);
-        });
-      
+      .then(res => res.json())
+      .then(current => {
+        setWeather(current);
+        setQuery('');
+        showHome(false); showNav(true); showResults(true);
+      });
     }
+  }
+
+  const searchBtn = evt => {
+    fetch(`${api.base}forecast?q=${query},US&units=imperial&APPID=${api.key}`)
+        .then(res =>res.json())
+        .then(future => {
+          setFuture(future);
+        })
+    fetch(`${api.base}weather?q=${query},US&units=imperial&APPID=${api.key}`)
+      .then(res =>res.json())
+      .then(current => {
+        setWeather(current);
+        setQuery('');
+        showHome(false); showNav(true); showResults(true);
+      })
   }
   
   // GET DATES
@@ -45,27 +65,26 @@ function App() {
     let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`
-  }  
+  } 
   
-
   return (
 
     <div className="app">
 
       {searchNav ? <nav className="navbar justify-content-between bg-light">
-        <div className="navLogo">
-          WA
+        <div className="col-3 homeTitle">
+          <div className="navLogo" onClick={goHome}>RW</div>
         </div>
-        <div className="navRight col-9 d-flex justify-content-evenly align-items-center">
+        <div className="navRight col-9 d-flex justify-content-start align-items-center">
           <input
             type="text"
             placeholder="Search by City or Zip Code"
-            className="search-bar col-7 text-center"
+            className="search-bar col-6 text-center"
             onChange={e => setQuery(e.target.value)}
-            value={query}
+            value={query}s
             onKeyPress={search}
           />
-          <button className="searchButton"><i className="fas fa-search"></i></button>
+          <button className="searchButton" onClick={searchBtn}><i className="fas fa-search"></i></button>
         </div>
       </nav> : null}
 
@@ -77,7 +96,7 @@ function App() {
         {home ? <section className="home">
 
             <div className="container text-center">
-              <h1 id="homeTitle">Weather App</h1>
+              <h1 className="homeTitle">Reactive Weather</h1>
 
               <input
                 type="text"
@@ -87,13 +106,17 @@ function App() {
                 value={query}
                 onKeyPress={search}
               />
-              <button className="searchBtn"
-                onClick={search}>
+            
+              <div className="error col-7 d-flex justify-content-center" id="error">
+                <span className="errorMsg">
+                  Error! Location not found. Please enter a US zip code or city name or check your spelling
+                </span>
+              </div>
+              <button className="searchButton" onClick={searchBtn}>
                 SEARCH
-                </button>
+              </button>
             </div>
         </section> : null}
-
         
 
         {/* ------------ */}
@@ -101,17 +124,24 @@ function App() {
         {/* ------------ */}
         {results ? <section className="results">
           <div className="container-fluid text-center">
-            <h2 className="location">{weather.name}, {weather.cod}, {weather.sys.country}</h2>
+            <h2 className="location">{weather.name}, {weather.sys.country}</h2>
             <div className="date">{dateBuilder(new Date())}</div>
+
+            {/* BEGIN CURRENT WEATHER CARD */}
             <div className="card">
-              <div className="card__image">
-                <div className="row d-block">
-                  <h4>Current</h4>
-                  <span className="curTemp">{Math.round(weather.main.temp)}&deg;F</span>
-                </div>
+              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon' className='weather-icon'/>
+              <div className="card__image currentText w-100">
+                <h4>Current</h4>
+                <span className="curTemp">{Math.round(weather.main.temp)}&deg;F</span>
                 <div className="row">
-                  <h5>Feels Like</h5>
-                  <span className="feels">{Math.round(weather.main.feels_like)}&deg;F</span>
+                  <div className="col-6">
+                    <h5>Feels Like</h5>
+                    <span className="feels">{Math.round(weather.main.feels_like)}&deg;F</span>
+                  </div>
+                  <div className="col-6">
+                    <h5>Condition</h5>
+                    <span className="feels">{weather.weather[0].main}</span>
+                  </div>
                 </div>
                 {/* FIND WEATHER IMAGES FOR SUNNY CLOUDY ETC */}
               </div>
@@ -149,7 +179,104 @@ function App() {
               </div>
             </div>
           </div>
-        </section>:null}
+          {/* END CURRENT CARD */}
+
+          {/* BEGIN 5 DAY CARD */}
+          <div className="dayBanner col-10 m-auto mt-3">
+            <div className="row d-flex justify-content-evenly">
+              <div className="day col-12 col-md-2">
+                <div className="container text-center">
+                  <div><h4>Tomorrow</h4></div>
+                  <div className="row">
+                    <div className="col-4">
+                      <h5>High</h5>
+                      <span>{Math.round(futureWeather.list[3].main.temp_max)}&deg;F</span>
+                    </div>
+                    <div className="col-4">
+                      <img src={`http://openweathermap.org/img/wn/${futureWeather.list[2].weather[0].icon}@2x.png`} alt='weather icon'/>
+                    </div>
+                    <div className="col-4">
+                      <h5>Low</h5>
+                      <span>{Math.round(futureWeather.list[0].main.temp_min)}&deg;F</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="day col-12 col-md-2">
+                <div className="container text-center">
+                  <div><h4>{futureWeather.list[6].dt_txt.substring(0, 10)}</h4></div>
+                  <div className="row">
+                    <div className="col-4">
+                      <h5>High</h5>
+                      <span>{Math.round(futureWeather.list[11].main.temp_max)}&deg;F</span>
+                    </div>
+                    <div className="col-4">
+                      <img src={`http://openweathermap.org/img/wn/${futureWeather.list[10].weather[0].icon}@2x.png`} alt='weather icon'/>
+                    </div>
+                    <div className="col-4">
+                      <h5>Low</h5>
+                      <span>{Math.round(futureWeather.list[8].main.temp_min)}&deg;F</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="day col-12 col-md-2">
+                <div className="container text-center">
+                  <div><h4>{futureWeather.list[14].dt_txt.substring(0, 10)}</h4></div>
+                  <div className="row">
+                    <div className="col-4">
+                      <h5>High</h5>
+                      <span>{Math.round(futureWeather.list[19].main.temp_max)}&deg;F</span>
+                    </div>
+                    <div className="col-4">
+                      <img src={`http://openweathermap.org/img/wn/${futureWeather.list[18].weather[0].icon}@2x.png`} alt='weather icon'/>
+                    </div>
+                    <div className="col-4">
+                      <h5>Low</h5>
+                      <span>{Math.round(futureWeather.list[16].main.temp_min)}&deg;F</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="day col-12 col-md-2">
+                <div className="container text-center">
+                  <div><h4>{futureWeather.list[22].dt_txt.substring(0, 10)}</h4></div>
+                  <div className="row">
+                    <div className="col-4">
+                      <h5>High</h5>
+                      <span>{Math.round(futureWeather.list[27].main.temp_max)}&deg;F</span>
+                    </div>
+                    <div className="col-4">
+                      <img src={`http://openweathermap.org/img/wn/${futureWeather.list[26].weather[0].icon}@2x.png`} alt='weather icon'/>
+                    </div>
+                    <div className="col-4">
+                      <h5>Low</h5>
+                      <span>{Math.round(futureWeather.list[24].main.temp_min)}&deg;F</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="day col-12 col-md-2">
+                <div className="container text-center">
+                  <div><h4>{futureWeather.list[30].dt_txt.substring(0, 10)}</h4></div>
+                  <div className="row">
+                    <div className="col-4">
+                      <h5>High</h5>
+                      <span>{Math.round(futureWeather.list[35].main.temp_max)}&deg;F</span>
+                    </div>
+                    <div className="col-4">
+                      <img src={`http://openweathermap.org/img/wn/${futureWeather.list[34].weather[0].icon}@2x.png`} alt='weather icon'/>
+                    </div>
+                    <div className="col-4">
+                      <h5>Low</h5>
+                      <span>{Math.round(futureWeather.list[32].main.temp_min)}&deg;F</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section> : null}
       </main>
 
 
